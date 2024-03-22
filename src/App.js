384f7e3ad12089199bad72c06746f96b258c6882
Chandan-MG <chandanmg.98@gmail.com>
2024-03-22 20:59:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -27,3 +27,70 @@ const App = () => {
 }
 
 export default App;
+
+
+
+// import './App.css';
+// import React, {useState} from 'react';
+
+// const DUMMY_DATA =[
+//   { text: 'Do all exercises', id:'g1'},
+//   { text: 'Complete the course', id:'g2'}
+// ]
+// function App() {
+
+//   const [ goals, setGoals] = useState(DUMMY_DATA)
+
+//   const [enteredGoal, setEnteredGoal] = useState('');
+
+//   const submitFormHandler = (event) =>{
+//     event.preventDefault();
+
+//     if(enteredGoal.trim() !== ''){
+//       const newGoal ={
+//         id: Math.random().toString(),
+//         text: enteredGoal.trim()
+//       }
+//       setGoals(prevGoals => [...prevGoals, newGoal]);
+//       setEnteredGoal('');
+//     }
+
+//     // console.log(enteredGoal);
+
+//   }
+
+//   const textChangedHandler = (event) => {
+//     setEnteredGoal(event.target.value);
+//   }
+
+
+//   return (
+//     <div>
+//       <div className="app">
+//         <form onSubmit={submitFormHandler}>
+//             <div className='card'>
+//               <div className='new-goal-controls'>
+//                 <div className='new-goal-control'>
+//                   <label id='text'>Course Goal</label>
+//                   <input type='text' id='text' value={enteredGoal} onChange={textChangedHandler} />
+//                 </div>
+//               </div>
+//               <div className='new-goal-button'>
+//                   <button type='submit'>Add Goal</button>
+//               </div>
+//             </div>
+//         </form>
+//       </div>
+      
+//       <div className='content'>
+//         {goals.map(goal => (
+//           <div key={goal.id} className='goal-item'>
+//             {goal.text}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
